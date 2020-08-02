ea13e094612de24255c459bccea70f759028238e
eric <ericarc@gmail.com>
2020-08-02 17:12:35 +0800

    Initial commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            由<a href="https://github.com/ericgame" target="_blank">Eric</a> 設計 <span style="color: #e27575;font-size: 14px;">★</span>
+        </p>
+        <p class="float-right"><a href="mailto:{{ setting('contact_email') }}">聯絡我們</a></p>
+    </div>
+</footer>
