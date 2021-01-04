f8a63a032a070aa189ee14b6e05d3b185b41f1c4
eric <ericarc99@gmail.com>
2021-01-04 15:41:25 +0000

    upload

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -10,6 +10,7 @@ use App\Models\Category;
 use App\Models\User;
 use App\Models\Link;
 use Auth;
+use App\Handlers\ImageUploadHandler;
 
 class TopicsController extends Controller
 {
@@ -40,6 +41,23 @@ class TopicsController extends Controller
         return view('topics.show', compact('topic'));
     }
 
+    //新建話題:view
+    public function create(Topic $topic)
+    {
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
+
+    //新建話題:儲存資料
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
+
+        return redirect()->to($topic->link())->with('success', '建立話題成功！');
+    }
+
     //文章編輯
     public function edit(Topic $topic)
     {
@@ -54,7 +72,7 @@ class TopicsController extends Controller
         $this->authorize('update', $topic);
         $topic->update($request->all());
 
-        return redirect()->to($topic->link())->with('success', '更新成功！');
+        return redirect()->to($topic->link())->with('success', '編輯成功！');
     }
 
     //文章刪除
@@ -66,4 +84,29 @@ class TopicsController extends Controller
         return redirect()->route('topics.index')->with('success', '刪除成功！');
     }
 
+    //新建話題:上傳圖片
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
+        //初始化返回資料，默認是失敗的
+        $data = [
+            'success' => false,
+            'msg' => '上傳失敗！',
+            'file_path' => '',
+        ];
+
+        //判斷是否有上傳檔案，並賦值給 $file
+        if($file = $request->upload_file){
+            //保存圖片到主機(本地端)
+            $result = $uploader->save($request->upload_file, 'topics', \Auth::id(), 1024);
+
+            //圖片保存成功
+            if($result){
+                $data['file_path'] = $result['path'];
+                $data['msg'] = '上傳成功！';
+                $data['success'] = true;
+            }
+        }
+
+        return $data;
+    }
 }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,20 +2,28 @@
 
 namespace App\Observers;
 
+use Illuminate\Support\Str;
 use App\Models\Topic;
+use Stichoza\GoogleTranslate\GoogleTranslate;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
+    public function saving(Topic $topic)
     {
-        //
-    }
+        //XSS過濾
+        $topic->body = clean($topic->body, 'user_topic_body');
 
-    public function updating(Topic $topic)
-    {
-        //
+        //產生話題摘錄
+        $topic->excerpt = make_excerpt($topic->body);
+
+        //產生網址slug (例如: 把 "測試翻譯slug" 翻譯成 test-translation-slug)
+        if(!$topic->slug){
+            //GoogleTranslate::trans($topic->title:要翻譯的資料, 'en':將資料翻譯成英語, null:自動偵測要翻譯的資料的語言)
+            //Str::slug 將英文句子的空格替換為"-"(例如: 把 "test translation slug" 轉成 test-translation-slug)
+            $topic->slug = Str::slug(GoogleTranslate::trans($topic->title, 'en', null));
+        }
     }
 }
